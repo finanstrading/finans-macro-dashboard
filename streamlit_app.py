7fd10e02b06e8e0156ab223b9d2b4ac159ec1a75
finanstrading <finanstrading10@gmail.com>
2026-07-15 08:50:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from urllib.parse import quote
 
 # ===================================================
 # CONFIGURACIÓN GENERAL
-# ===================================================
+# =================================================== 
 
 st.set_page_config(
     page_title="Finans Trading | Fundamental Dashboard",
