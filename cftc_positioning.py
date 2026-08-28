06881f4df5fb5c3888b6d3785182e95737f0cf3b
finanstrading <finanstrading10@gmail.com>
2026-08-28 14:47:16 +0100

    Update cftc_positioning.py

--- cftc_positioning.py
+++ cftc_positioning.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 import streamlit as st
+import plotly.graph_objects as go
 
 # ============================================================
 # CONFIGURACIÓN CFTC
