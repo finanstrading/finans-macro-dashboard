8be6dc261499f4698391131e4bfdf408485876ef
finanstrading <finanstrading10@gmail.com>
2026-08-30 15:47:09 +0100

    Update auth.py

--- auth.py
+++ auth.py
@@ -695,7 +695,7 @@ def require_authenticated_user():
             "persistent_cookie_token"
         ] = pending_token
 
-        st.rerun()
+   
 
     # --------------------------------------------------------
     # 2. Sesión normal de Streamlit
