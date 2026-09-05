1e384cef909d8fb8e741c87d6619b17387598553
finanstrading <finanstrading10@gmail.com>
2026-09-05 14:31:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import requests
 import plotly.graph_objects as go
 from urllib.parse import quote  
 
-from auth import require_authenticated_user, render_logout
+from auth import require_authenticated_user, render_logout 
 from monetary_engine import analizar_indicador, ENGINE_VERSION  
 
 from currency_score_engine import (
